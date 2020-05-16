5b795309020f5a4a2404c7d77bc981d6c2a1b397
abersoz <abersoz@gmail.com>
2020-05-16 23:02:03 +0300

    thinking more simpliest way

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Game from './Game';
 
+const steps = [
+  {
+    question: 'Question 1',
+    answers: 'Answer 1'
+  },
+  {
+    question: 'Question 2',
+    answers: 'Answer 2'
+  },
+  {
+    question: 'Question 3',
+    answers: 'Answer 3'
+  }
+];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello <Game/>
-      </header>
+    <div>
+      <div>WEBCAM</div>
+      <div>PANEL</div>
     </div>
   );
 }
--- src/Game.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Game() {
-    return (
-        <span>try not to laugh game...</span>
-    );
-}
-
-export default Game;
