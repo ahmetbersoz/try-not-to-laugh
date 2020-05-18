836fd8869cdd09f76ef21982c20e3bdd826ee3b1
abersoz <abersoz@gmail.com>
2020-05-18 17:12:04 +0300

    facedetector is included

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import * as faceapi from 'face-api.js'
 
 const stepsDB = [
 	{
@@ -46,6 +47,40 @@ function App() {
 	}, [remainingTimeStarted, remainingTime])
 
 
+	//getting video stream
+	const video = document.getElementById('video');
+
+	Promise.all([
+		faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+		faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+		faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+		faceapi.nets.faceExpressionNet.loadFromUri('./models')
+	]).then(startVideo);
+
+	function startVideo() {
+		navigator.getUserMedia(
+			{ video: {} },
+			stream => video.srcObject = stream,
+			err => console.error(err)
+		)
+	}
+
+	video.addEventListener('play', () => {
+		const canvas = faceapi.createCanvasFromMedia(video)
+		document.body.append(canvas)
+		const displaySize = { width: video.width, height: video.height }
+		faceapi.matchDimensions(canvas, displaySize)
+		setInterval(async () => {
+			const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+			const resizedDetections = faceapi.resizeResults(detections, displaySize)
+			canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+			faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+			faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+		}, 100)
+	})
+
+
+
 	function start() {
 		steps = stepsDB.slice(0);
 		const randomIndex = Math.floor(Math.random() * steps.length);
@@ -60,6 +95,11 @@ function App() {
 	}
 
 	function goNextStep() {
+		if (steps.length === 0) {
+			setGameStatus('win');
+			return;
+		}
+
 		const randomIndex = Math.floor(Math.random() * steps.length);
 		const nextStep = steps.splice(randomIndex, 1).pop();
 		setStep(nextStep);
@@ -67,7 +107,6 @@ function App() {
 
 	return (
 		<div>
-			<div>WEBCAM</div>
 			<div>
 				{gameStatus === 'initial' &&
 					<button onClick={start}>Start</button>
