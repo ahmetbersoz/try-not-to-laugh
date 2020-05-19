274d82beda9d8dc2d10a54b5819f56dfa42c8ed4
abersoz <abersoz@gmail.com>
2020-05-19 23:20:11 +0300

    Game logic is finished

--- src/App.js
+++ src/App.js
@@ -20,66 +20,70 @@ const stepsDB = [
 ];
 let steps;
 
+const video = document.getElementById('video');
+
+Promise.all([
+	faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+	faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo);
+
+function startVideo() {
+	navigator.getUserMedia(
+		{ video: {} },
+		stream => video.srcObject = stream,
+		err => console.error(err)
+	)
+}
+
 function App() {
-	const [gameStatus, setGameStatus] = useState('initial'); //'initial', 'playing, 'win', 'lose'
-	const [smileStatus, setSmileStatus] = useState(false); //true, false
+	const timeStep = 5000; //milliseconds
+	const [gameStatus, setGameStatus] = useState('initial'); //'initial', 'playing, 'win', 'fail'
+	const [smilePercent, setSmilePercent] = useState(0); //0-1
 	const [step, setStep] = useState({ id: 0, question: '', answer: '' });
 	const [answerVisibility, setAnswerVisibility] = useState(false); //true, false
-	const [remainingTime, setRemainingTime] = useState(5);
+	const [remainingTime, setRemainingTime] = useState(timeStep);
 	const [remainingTimeStarted, setRemainingTimeStarted] = useState(false);
 
 	useEffect(() => {
 		let timer;
+		if (gameStatus === 'initial' || gameStatus === 'playing' ) {
+			timer = setInterval(async () => {
+				const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
+				
+				if (detections.length) {
+					detections.forEach(detection => {
+						setSmilePercent(detection.expressions.happy);
+
+						if (smilePercent > 0.5 && gameStatus === 'initial') start();
+						if (smilePercent > 0.2 && remainingTimeStarted) setGameStatus('fail');
+
+					});
+				}
+			}, 100);
+		} else {
+			clearInterval(timer);
+		}
+		return () => clearInterval(timer);
+	}, [gameStatus, smilePercent])
+
+	useEffect(() => {
+		let timer;
 		if (remainingTimeStarted) {
-			timer = setInterval(() => {
-				setRemainingTime(remainingTime => remainingTime - 1);
+			timer = setInterval(async () => {
 				if (remainingTime === 0) {
 					setAnswerVisibility(false);
 					setRemainingTimeStarted(false);
-					setRemainingTime(5);
+					setRemainingTime(timeStep);
 					goNextStep();
 				}
-			}, 1000);
+
+				setRemainingTime(remainingTime => remainingTime - 100);
+			}, 100);
 		} else {
 			clearInterval(timer);
 		}
 		return () => clearInterval(timer);
 	}, [remainingTimeStarted, remainingTime])
-
-
-	//getting video stream
-	const video = document.getElementById('video');
-
-	Promise.all([
-		faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-		faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
-		faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-		faceapi.nets.faceExpressionNet.loadFromUri('./models')
-	]).then(startVideo);
-
-	function startVideo() {
-		navigator.getUserMedia(
-			{ video: {} },
-			stream => video.srcObject = stream,
-			err => console.error(err)
-		)
-	}
-
-	video.addEventListener('play', () => {
-		const canvas = faceapi.createCanvasFromMedia(video)
-		document.body.append(canvas)
-		const displaySize = { width: video.width, height: video.height }
-		faceapi.matchDimensions(canvas, displaySize)
-		setInterval(async () => {
-			const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-			const resizedDetections = faceapi.resizeResults(detections, displaySize)
-			canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-			faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-			faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-		}, 100)
-	})
-
-
 
 	function start() {
 		steps = stepsDB.slice(0);
@@ -108,17 +112,20 @@ function App() {
 	return (
 		<div>
 			<div>
+				<div>Smile level: {smilePercent.toFixed(5)}</div>
 				{gameStatus === 'initial' &&
-					<button onClick={start}>Start</button>
+					<span>Smile to start!</span>
 				}
 				{gameStatus === 'playing' &&
 					<div>
 						<div id="question">{step.question}</div>
 						<button onClick={() => showAnswer()}>Show Answer</button>
 						{answerVisibility &&
-							<div id="answer">{step.answer}</div>
+							<div>
+								<div id="answer">{step.answer}</div>
+								<div>Remaining time: {(remainingTime/1000).toFixed(0) > 0 ? (remainingTime/1000).toFixed(0) : 0}</div>
+							</div>
 						}
-						<div>Remaining time: {remainingTime}</div>
 					</div>
 				}
 				{gameStatus === 'win' &&
@@ -127,9 +134,9 @@ function App() {
 						<button onClick={start}>Play Again</button>
 					</div>
 				}
-				{gameStatus === 'lose' &&
+				{gameStatus === 'fail' &&
 					<div>
-						<div>Congrats! You lose</div>
+						<div>Ooops! You fail</div>
 						<button onClick={start}>Play Again</button>
 					</div>
 				}
